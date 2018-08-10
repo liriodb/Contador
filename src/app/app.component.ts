198d2e106d0eb552ff2cce947ba891989eea2c3d
liriodb <liriors2.0@gmail.com>
2018-08-10 18:56:50 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+
+export class AppComponent {
+  appValor: number = 3;
+
+  eventListener(event): void {
+    this.appValor = event;
+  }
+}
